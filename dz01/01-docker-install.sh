0b6e192642db296eb56aa80fb66dbc8a3c36e406
Neor5804 <neor5804@gmail.com>
2019-09-19 14:57:08 +0000

    update

--- dz01/01-docker-install.sh
+++ dz01/01-docker-install.sh
@@ -5,52 +5,63 @@ echo -e "#		Web Hosting implementation Script	       #"
 echo -e "#		Written By: Vadim Bykau			       #"
 echo -e "#		Updated: 19.09.2019			       #"
 echo -e "################################################################"
-#
+
 #Install Docker Engine - Community for Ubuntu 18.04
-echo
-read -r -p "Delete old Docker versions? [Y/n]" -n 1
+echo Hello $USER && echo
+read -r -p "Would you like delete old Docker versions? [Y/n]" -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-        echo -e "Old versions will be deleted"
+        echo && echo -e '\033[31mOld versions will be deleted\033[m' && echo
 	sudo apt-get remove docker docker-engine docker.io containerd runc
 fi
-echo
-echo -e "Install Docker Engine - Community using the repository:"
-echo -e "Update the apt package index..."
+echo && echo -e '\033[32mInstall Docker Engine - Community using the repository\033[m' && echo
+echo -e "Update the apt package index..." && echo
 sudo apt-get update
-echo -e "Install packages to allow apt to use a repository over HTTPS"
+echo -e "Install packages to allow apt to use a repository over HTTPS" && echo
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-echo -e "Add Docker’s official GPG key..."
+echo -e "Add Docker’s official GPG key..." && echo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-echo -e "Set up the stable repository"
+echo -e "Set up the stable repository" && echo
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-echo -e "Update the apt package index..."
+echo && echo -e "Update the apt package index..." && echo
 sudo apt-get update
-echo "Install the latest version of Docker Engine - Community and containerd..."
+echo && echo -e '\033[32mInstall the latest version of Docker Engine - Community and containerd...\033[m' && echo
 sudo apt-get install docker-ce docker-ce-cli containerd.io
-echo "Start the Docker daemon"
+echo && echo "Start the Docker daemon" && echo
 sudo systemctl start docker
-sleep 3
+sleep 10
+
 #Install Docker Compose
-echo "Download the current stable release of Docker Compose - 1.24.1"
+echo "Download the current stable release of Docker Compose - 1.24.1" && echo
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-echo "Apply executable permissions to the binary..."
+echo && echo "Apply executable permissions to the binary..." && echo
 sudo chmod +x /usr/local/bin/docker-compose
-sleep 3
-#Post-installation step - Manage Docker as a non-root user
+echo $USER
+sleep 10
+read -p "Press enter to continue"
+
+#Add user to docker group
+echo && echo -e '\033[32mPost-installation step - Manage Docker as a non-root user\033[m' && echo
 echo "Create the docker group."
 sudo groupadd docker
+echo $USER
 echo "Add your user to the docker group."
 sudo usermod -aG docker $USER
-sudo newgrp docker
+echo $USER
+read -p "Press enter to continue"
+echo "activate the changes to groups"
+#sudo newgrp docker
+echo $USER
+sleep 10
+read -p "Press enter to continue"
 
 #Test Docker installation
-echo "Test the Docker installation. Get version and etc..."
+echo && echo -e '\033[32mTest the Docker installation. Get version and etc...\033[m' && echo
 docker-compose --version
 #Start test-container "Hello world"
 docker run hello-world
